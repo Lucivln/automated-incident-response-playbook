f6374fab287d47cfea57a2d17e0b07baa2cacdf8
root <root@DESKTOP-8194M1D.localdomain>
2025-10-21 14:31:33 +0530

    fix: Final structural fix for Terraform containment data lookup.

--- aws/terraform/containment.tf
+++ aws/terraform/containment.tf
@@ -1,4 +1,4 @@
-# aws/terraform/containment.tf (Replace existing content)
+# aws/terraform/containment.tf (Ensure this is the final, correct content)
 
 # 1. Look up the existing target instance
 data "aws_instance" "target_host_lookup" {
@@ -7,10 +7,13 @@ data "aws_instance" "target_host_lookup" {
     values = ["Wazuh-Target"] 
   }
   instance_state_names = ["running"]
+  # NOTE: If this fails, the host might not be fully "running" when the job starts.
 }
 
 # 2. Use the instance attachment resource for reliable containment
 resource "aws_instance_sg_attachment" "quarantine_attachment" {
+  # Use the simple instance ID from the lookup
   instance_id        = data.aws_instance.target_host_lookup.id
+  # Use the ID of the Quarantine Security Group
   security_group_id  = aws_security_group.quarantine_sg.id
 }
