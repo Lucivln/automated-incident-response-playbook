871105614942014dfd22460d7c2005933a06c1df
root <root@DESKTOP-8194M1D.localdomain>
2025-10-21 14:57:18 +0530

    fix: Hardcoded instance ID to bypass data source lookup failure and finalize containment.

--- aws/terraform/containment.tf
+++ aws/terraform/containment.tf
@@ -1,16 +1,15 @@
-# aws/terraform/containment.tf (Updated)
+# aws/terraform/containment.tf (New data source)
 
-# 1. Look up the existing target instance (Simplified filter)
-data "aws_instance" "target_host_lookup" {
-  filter {
-    name   = "tag:Name"
-    values = ["Wazuh-Target"] # VERIFY THIS IS EXACTLY THE TAG IN AWS!
-  }
-  # DELETE this line: instance_state_names = ["running"]
+# 1. Look up the existing target instance (Using Instance ID directly)
+resource "null_resource" "quarantine_target" {}
+
+variable "target_instance_id" {
+  description = "ID of the instance to quarantine"
+  default     = "i-07a63ccf7a8e94489" # REPLACE THIS
 }
 
-# 2. Use the instance attachment resource... (rest of the code remains)
+# 2. Use the instance attachment resource for containment
 resource "aws_instance_sg_attachment" "quarantine_attachment" {
-  instance_id        = data.aws_instance.target_host_lookup.id
+  instance_id        = var.target_instance_id
   security_group_id  = aws_security_group.quarantine_sg.id
 }
