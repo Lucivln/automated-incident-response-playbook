b43463f330a1dd3b6c22acd4658565abf69b930d
root <root@DESKTOP-8194M1D.localdomain>
2025-10-21 15:09:56 +0530

    fix: Added versions.tf to satisfy Terraform CI/CD runner requirements.

---
+++ aws/terraform/versions.tf
@@ -0,0 +1,10 @@
+# aws/terraform/versions.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
