37ca0dda194523ca2abc5200eb8198a5d6ef4b54
root <root@DESKTOP-8194M1D.localdomain>
2025-10-21 14:19:24 +0530

    fix: Created missing forensics.yml and implemented final robust containment.tf fix.

--- aws/terraform/containment.tf
+++ aws/terraform/containment.tf
@@ -4,13 +4,12 @@
 data "aws_instance" "target_host_lookup" {
   filter {
     name   = "tag:Name"
-    values = ["Wazuh-Target"]
+    values = ["Wazuh-Target"] 
   }
   instance_state_names = ["running"]
 }
 
-# 2. Use the instance attachment resource for containment
-# This is a simpler and more reliable resource for attaching a single SG.
+# 2. Use the instance attachment resource for reliable containment
 resource "aws_instance_sg_attachment" "quarantine_attachment" {
   instance_id        = data.aws_instance.target_host_lookup.id
   security_group_id  = aws_security_group.quarantine_sg.id
